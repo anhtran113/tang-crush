bfd69f1a60131a3bb358676d8971312befbc8eab
anhtran113 <88124554+anhtran113@users.noreply.github.com>
2021-07-29 10:41:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Mình à!',
+    introDesc: `Tui biết là dạo này
+    mình có nhiều đám mây đen chắn lối
+    Tuy tui không thể ở cạnh mình
+    cùng mình xua đi mây đen nhưng mà `,
+    btnIntro: 'tui thương mình',
+    title: 'Halo 小云! mình là mớ mây đen đeo bám cậu đây 😙',
+    desc: 'Mình biết mình gần đây đem đến không ít mệt mỏi đến cho cậu ',
+    btnYes: 'Hơi phiền nhưng tạm chấp nhận',
+    btnNo: 'Đi lẹ dùm tui đi mấy baaa',
+    question:'Mây đen hỏi nhỏ 小云 có mệt lắm không ạ?',
+    btnReply: 'Gửi mây đen',
+    reply: 'Mệt nhiều nhưng 小云 biết có người luôn thương và lo lắng cho mình',
+    mess: 'Tui biết đám mây nhỏ của tui dạo gần đây chất chứa nhiều mệt mỏi. Đoạn đường này mình hãy nhớ là luôn có tui ở cạnh bên luôn mong những an yên đến với tụi mình',
+    messDesc: '小云 biết chi hong, em cười rất xinh á. Thế nên 小云 hãy cười thật tươi và cùng tui đi tiếp nhá',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }

48b9f928e3f018a15c26dbd5e5d6bdb618191479
anhtran113 <88124554+anhtran113@users.noreply.github.com>
2021-07-29 10:50:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const CONFIG = {
     introTitle: 'Mình à!',
     introDesc: `Tui biết là dạo này
     mình có nhiều đám mây đen chắn lối
-    Tuy tui không thể ở cạnh mình
-    cùng mình xua đi mây đen nhưng mà `,
+    còn vì COVID mà tui không thể ở cạnh mình lúc này
+    cùng mình xua đi mây đen nhưng mà.. `,
     btnIntro: 'tui thương mình',
     title: 'Halo 小云! mình là mớ mây đen đeo bám cậu đây 😙',
     desc: 'Mình biết mình gần đây đem đến không ít mệt mỏi đến cho cậu ',
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Đi lẹ dùm tui đi mấy baaa',
     question:'Mây đen hỏi nhỏ 小云 có mệt lắm không ạ?',
     btnReply: 'Gửi mây đen',
-    reply: 'Mệt nhiều nhưng 小云 biết có người luôn thương và lo lắng cho mình',
-    mess: 'Tui biết đám mây nhỏ của tui dạo gần đây chất chứa nhiều mệt mỏi. Đoạn đường này mình hãy nhớ là luôn có tui ở cạnh bên luôn mong những an yên đến với tụi mình',
-    messDesc: '小云 biết chi hong, em cười rất xinh á. Thế nên 小云 hãy cười thật tươi và cùng tui đi tiếp nhá',
+    reply: 'Mệt nhiều nhưng 小云 biết có người yêu supper cute luôn thương và lo lắng cho mình',
+    mess: 'Tui biết 小云 của tui dạo gần đây chất chứa nhiều mệt mỏi. Đoạn đường này mình hãy nhớ là luôn có tui ở cạnh bên luôn mong có thể cùng mình đi qua những khó khăn như bây giờ',
+    messDesc: '小云 biết chi hong, em cười rất xinh á. Thế nên lúc này 小云 đừng ngại chi hãy cười thật tươi và cùng tui đi tiếp nhá',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
